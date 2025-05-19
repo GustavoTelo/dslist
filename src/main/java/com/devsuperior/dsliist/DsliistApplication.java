4903712d38e3c11fbca1a193117985a5c9150c66
Gustavo <gustavotelo007@gmail.com>
2025-05-19 20:17:16 -0300

    project created

---
+++ src/main/java/com/devsuperior/dsliist/DsliistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsliist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsliistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsliistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsliist/DsliistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsliist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsliistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
